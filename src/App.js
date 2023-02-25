f873909ef3e5793e2c7cad84d048c6a6429587da
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-02-25 14:07:13 +0530

    added routes for portfolio and blog pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Blog from "./Pages/Blog/Blog";
 import LandingPage from "./Pages/LandingPage";
+import Portfolio from "./Pages/Portfolio/Portfolio";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<LandingPage />} />
+      <Route exact path="/portfolio" element={<Portfolio />} />
+      <Route exact path="/blog" element={<Blog />} />
     </Routes>
   );
 }
